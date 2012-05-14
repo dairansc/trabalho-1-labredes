81a0c7bd93e52e126ba3ae67319a34f6a6aee88d
bruno.campiol <bruno.campiol@gmail.com>
2012-05-14 21:01:58 +0000

    Versao Inicial

---
+++ codigos/Socket.h
@@ -0,0 +1,14 @@
+#ifndef _Socket_
+#define _Socket_
+
+#include <stdlib.h> /* for exit() */
+
+/* Error handling function */
+void DieWithError(char *errorMessage)
+{
+    perror(errorMessage);
+    exit(1);
+}
+
+
+#endif
