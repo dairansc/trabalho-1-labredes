ccfd2b7fdaa1a17f4d5b8c41520ab38ce8cfdc15
dairansc@gmail.com <dairansc@gmail.com@7f988166-9d74-25f5-e823-d3f049de0fd6>
2012-05-22 17:22:22 +0000

    leitura dos cabeçalhos até ICMP, inserção de lista encadeada

---
+++ trunk/codigos/main.c
@@ -0,0 +1,131 @@
+// Trabalho 1 - LabRedes 2012 - Bruno e Dairan
+
+
+// http://www.mutekh.org/www/mutekh_api/Headers_documentation.html
+
+/* Includes */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <unistd.h>
+
+/* Diretórios: net, netinet, linux contém os includes que descrevem */
+/* as estruturas de dados do header dos protocolos   		    */
+/* Cuidado: se uitlizar netinet não uitlize o diretório linux e     */
+/* vice-versa						            */
+
+#include <net/if.h>  //estrutura ifr
+#include <netinet/ether.h> //header ethernet
+#include <netinet/in.h> //definição de protocolos
+#include <arpa/inet.h> //funções para manipulação de endereços IP
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/in_systm.h> //tipos de dados
+
+#include "slist.c"
+
+#if !defined(IPVERSION)
+#define IPVERSION 4
+#endif
+
+#define BUFFSIZE 1500
+
+// Atencao!! Confira no /usr/include do seu sisop o nome correto
+// das estruturas de dados dos protocolos.
+
+  unsigned char buff1[BUFFSIZE]; // buffer de recepcao
+
+  int sockd;
+  int on;
+  struct ifreq ifr;
+
+typedef struct maquinas {
+    uint8_t mac[6];
+    uint32_t ip;
+
+} MAQUINAS;
+
+int main(int argc,char *argv[])
+{
+    int i;
+	struct ether_header *etherhdr; // struct de Ethernet
+	struct arphdr *arphdrptr; // struct ARP
+	struct iphdr *iphdrptr; // struct IP
+	struct icmphdr *icmphdrptr;
+
+	struct in_addr source, dest;
+
+	NODE *lista;
+	lista=list_create((void*)"");
+
+	sockd = socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
+	if(sockd < 0) {
+		perror("socket");
+		exit(-1);
+	}
+	printf("%d \n", sockd);
+
+	// O procedimento abaixo é utilizado para "setar" a
+	// interface em modo promíscuo
+
+	strcpy(ifr.ifr_name, "eth0");
+	if(ioctl(sockd, SIOCGIFINDEX, &ifr) < 0)
+		printf("erro no ioctl!");
+	ioctl(sockd, SIOCGIFFLAGS, &ifr);
+	ifr.ifr_flags |= IFF_PROMISC;
+	ioctl(sockd, SIOCSIFFLAGS, &ifr);
+
+	// recepção de pacotes
+	printf(":: Capturando pacotes ::\n");
+	while (1) {
+		recv(sockd,(char *) &buff1,sizeof(buff1),0x0);
+
+		// Protocolo Ethernet
+		etherhdr = (struct ether_header *)buff1;
+
+
+
+		if(ntohs(etherhdr->ether_type) == ETHERTYPE_IP){
+
+
+
+			if (iphdrptr->protocol == 0x01) {
+				icmphdrptr = (struct icmphdr *)(buff1+sizeof(struct ether_header)+iphdrptr->ihl*4);
+				if(icmphdrptr->type == ICMP_ECHO){
+                    printf("-------------  ==   == -------------\n");
+                    printf("       MAC Source: ");
+                    for(i=0; i<6; i++){
+                        printf("%02X:", etherhdr->ether_shost[i]);
+                    }
+                    printf("\n");
+                    printf("  MAC Destination: ");
+
+                    for(i=0; i<6; i++){
+                        printf("%02X:", etherhdr->ether_dhost[i]);
+                    }
+                    printf("\n             Type: %04X\n", ntohs(etherhdr->ether_type));
+
+
+                    printf("IP\n");
+                    iphdrptr = (struct iphdr *)(buff1+sizeof(struct ether_header));
+                    memcpy(&source, &iphdrptr->saddr, 4);
+                    memcpy(&dest, &iphdrptr->daddr, 4);
+                    printf("        IP Source: %s\n", inet_ntoa(source));
+                    printf("   IP Destination: %s\n", inet_ntoa(dest));
+
+                    printf("ICMP\n");
+                    //printf("\n               ID: %04X\n", ntohs(icmphdrptr->un & 0x0000FFFF);
+                    //printf("\n  Sequence number: %04X\n", ntohs((icmphdrptr->un & 0xFFFF0000) >> 16);
+                }
+			}
+		}/*
+		else if(ntohs(etherhdr->ether_type) == ETHERTYPE_ARP) {
+			printf("ARP\n");
+			arphdrptr = (struct arphdr *)(buff1+sizeof(struct ether_header));
+		}*/
+	}
+}
---
+++ trunk/codigos/slist.c
@@ -0,0 +1,133 @@
+/* Copyright (c) 2012 the authors listed at the following URL, and/or
+the authors of referenced articles or incorporated external code:
+http://en.literateprograms.org/Singly_linked_list_(C)?action=history&offset=20081126164854
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Retrieved from: http://en.literateprograms.org/Singly_linked_list_(C)?oldid=15494
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+typedef struct node_s {
+	void *data;
+	struct node_s *next;
+} NODE;
+
+NODE *list_create(void *data)
+{
+	NODE *node;
+	if(!(node=malloc(sizeof(NODE)))) return NULL;
+	node->data=data;
+	node->next=NULL;
+	return node;
+}
+NODE *list_insert_after(NODE *node, void *data)
+{
+	NODE *newnode;
+        newnode=list_create(data);
+        newnode->next = node->next;
+        node->next = newnode;
+	return newnode;
+}
+NODE *list_insert_beginning(NODE *list, void *data)
+{
+	NODE *newnode;
+        newnode=list_create(data);
+        newnode->next = list;
+	return newnode;
+}
+int list_remove(NODE *list, NODE *node)
+{
+	while(list->next && list->next!=node) list=list->next;
+	if(list->next) {
+		list->next=node->next;
+		free(node);
+		return 0;
+	} else return -1;
+}
+int list_foreach(NODE *node, int(*func)(void*))
+{
+	while(node) {
+		if(func(node->data)!=0) return -1;
+		node=node->next;
+	}
+	return 0;
+}
+NODE *list_find(NODE *node, int(*func)(void*,void*), void *data)
+{
+	while(node) {
+		if(func(node->data, data)>0) return node;
+		node=node->next;
+	}
+	return NULL;
+}
+int printstring(void *s)
+{
+	printf("%s\n", (char *)s);
+	return 0;
+}
+int findstring(void *listdata, void *searchdata)
+{
+	return strcmp((char*)listdata, (char*)searchdata)?0:1;
+}
+
+/*
+int main()
+{
+	NODE *list, *second, *inserted;
+	NODE *match;
+
+	// Create initial elements of list
+	list=list_create((void*)"First");
+	second=list_insert_after(list, (void*)"Second");
+	list_insert_after(second, (void*)"Third");
+
+	printf("Initial list:\n");
+	list_foreach(list, printstring);
+	putchar('\n');
+
+	// Insert 1 extra element in front
+	list=list_insert_beginning(list, "BeforeFirst");
+	printf("After list_insert_beginning():\n");
+	list_foreach(list, printstring);
+	putchar('\n');
+
+	// Insert 1 extra element after second
+	inserted=list_insert_after(second, "AfterSecond");
+	printf("After list_insert_after():\n");
+	list_foreach(list, printstring);
+	putchar('\n');
+
+	// Remove the element
+	list_remove(list, inserted);
+	printf("After list_remove():\n");
+	list_foreach(list, printstring);
+	putchar('\n');
+
+	// Search
+	if((match=list_find(list, findstring, "Third")))
+		printf("Found \"Third\"\n");
+	else printf("Did not find \"Third\"\n");
+
+	return 0;
+}
+*/
